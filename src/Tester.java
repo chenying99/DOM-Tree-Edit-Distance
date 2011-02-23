b12a38735bbf12e20c5e693a2224d3dc964b657c
shawntan <shawn@wtf.sg>
2011-02-23 17:29:53 +0800

    Finding TED for list of urls.

--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.imageio.stream.FileImageInputStream;
+
 import org.w3c.dom.Node;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import domted.DOMPostOrder;
@@ -9,33 +21,89 @@ import domted.TreeEditDistance;
 
 
 public class Tester {
+	private static BufferedReader reader;
+	private static PrintWriter writer;
+
 	public static void main(String[] args) {
-		WebClient client = new WebClient();
-		client.setJavaScriptEnabled(false);
+		if(args.length == 1) openFile(args[0]);
+		else System.out.println("NOOOO");
+		WebClient client = createClient();
+		TreeEditDistance ted = createTED();
+		String input;
+
+		DomNode prevBody=null;
+		HtmlPage page = null;
 		try {
-			HtmlPage p1 = client.getPage(
-					"http://www.novelr.com/"
-			);
-			HtmlPage p2 = client.getPage(
-					"http://www.wtf.sg"
-			);
-			TreeEditDistance ted = new TreeEditDistance(new NodeDistance() {
-				public int rename(Node n1, Node n2) {
-					if(n1.getNodeName().equals(n2.getNodeName())) return 0;
-					else return 1;
+			while((input=reader.readLine())!=null) {
+				try {
+					System.out.println("Downloading "+input+"....");
+					page = client.getPage(input);
+					System.out.println("Done.");
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-				public int insert(Node n1, Node n2) {
-					return 2;
+				if(page!= null){
+					if(prevBody != null){
+						if(page.getBody()== prevBody) continue;
+						writer.print(input);
+						writer.print("\t");
+						writer.print(ted.calculate(prevBody, page.getBody()));
+					}
 				}
-				public int delete(Node n1, Node n2) {
-					return 2;
-				}
-			});
-			int val = ted.calculate(p1.getBody(), p2.getBody());
-			System.out.println(val);
-			
-		} catch (Exception e) {
+				prevBody = page.getBody();
+				writer.println();
+				writer.flush();
+			}
+		} catch (FailingHttpStatusCodeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		closeFiles();
+	}
+
+	private static WebClient createClient(){
+		return WebClientFactory.borrowClient();
+	}
+	private static TreeEditDistance createTED(){
+		NodeDistance nd = new NodeDistance() {
+			public int rename(Node n1, Node n2) {
+				if(n1.getNodeName().equals(n2.getNodeName())) return 0;
+				else return 1;
+			}
+			public int insert(Node n1, Node n2) {
+				return 2;
+			}
+			public int delete(Node n1, Node n2) {
+				return 2;
+			}
+		};
+		return new TreeEditDistance(nd);		
+	}
+
+	private static void openFile(String s) {
+		File f = new File(s);
+		File dir = new File(f.getParent());
+
+		try {
+			File newName = new File(dir.getPath()+File.separatorChar+f.getName()+".log");
+			reader = new BufferedReader(new FileReader(f));
+			writer = new PrintWriter(newName);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	private static void closeFiles(){
+		try {
+			reader.close();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/domted/TreeEditDistance.java
+++ src/domted/TreeEditDistance.java
@@ -1,8 +1,5 @@
 package domted;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-
 import org.w3c.dom.Node;
 
 public class TreeEditDistance {
@@ -11,7 +8,6 @@ public class TreeEditDistance {
 		this.nd = nd;
 	}
 	public int calculate(Node n1,Node n2) {
-		NumberFormat formatter = new DecimalFormat("#00");
 		DOMPostOrder t1 = new DOMPostOrder(n1);
 		DOMPostOrder t2 = new DOMPostOrder(n2);
 		int[][] td = new int[t1.N.length][t2.N.length];
@@ -30,40 +26,36 @@ public class TreeEditDistance {
 		}*/
 		return td[td.length-1][td[0].length-1];
 	}
-	
+
 	private void forestDist(int i, 		int j,
-							Integer[] li,	Integer[] lj,
-							Node[] nodes1,Node[] nodes2,
-							int[][] td,int[][] fd) {
+			Integer[] li,	Integer[] lj,
+			Node[] nodes1,Node[] nodes2,
+			int[][] td,int[][] fd) {
 		fd[li[i]-1][lj[j]-1] = 0;
 		for(int di=li[i];di<=i;di++) fd[di][lj[j]-1] = fd[di-1][lj[j]-1]+nd.delete(nodes1[di], nodes2[lj[j]-1]);
 		for(int dj=lj[j];dj<=j;dj++) 
 			fd[li[i]-1][dj] = 
 				fd[li[i]-1][dj-1] 
-			+ nd.insert(
-					nodes1[li[i]-1],
-					nodes2[dj]);
+				            + nd.insert(
+				            		nodes1[li[i]-1],
+				            		nodes2[dj]);
 		for(int di=li[i];di<=i;di++) {
 			for(int dj=lj[j];dj<=j;dj++) {
 				if(li[di]==li[i] && lj[dj]==lj[j]) {
 					fd[di][dj] =
-					Math.min(		fd[di-1][dj] + nd.delete(nodes1[di], nodes2[dj]),
-						Math.min(	fd[di][dj-1] + nd.insert(nodes1[di], nodes2[dj]),
-									fd[di-1][dj-1] + nd.rename(nodes1[di], nodes2[dj])
+						Math.min(			fd[di-1][dj] + nd.delete(nodes1[di], nodes2[dj]),
+								Math.min(	fd[di][dj-1] + nd.insert(nodes1[di], nodes2[dj]),
+											fd[di-1][dj-1] + nd.rename(nodes1[di], nodes2[dj])
 								)
-					);
+						);
 					td[di][dj]=fd[di][dj];
 				} else 	fd[di][dj] =
-					Math.min(		fd[di-1][dj] + nd.delete(nodes1[di], nodes2[dj]),
-						Math.min(	fd[di][dj-1] + nd.insert(nodes1[di], nodes2[dj]),
-									fd[li[di]-1][lj[dj]-1] + td[di][dj]
+					Math.min(			fd[di-1][dj] + nd.delete(nodes1[di], nodes2[dj]),
+							Math.min(	fd[di][dj-1] + nd.insert(nodes1[di], nodes2[dj]),
+										fd[li[di]-1][lj[dj]-1] + td[di][dj]
 							)
 					);
 			}
 		}
-		
-		
 	}
-	
-
 }
